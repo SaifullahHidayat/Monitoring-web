9a5a69e307e1e4bd5207b69d24f7b9bb6b7c521b
SaifullahHidayat <syaifullah1108@gmail.com>
2025-12-03 17:54:45 +0700

    Icon

--- dashboard.php
+++ dashboard.php
@@ -28,6 +28,7 @@ $alert_type = 'warning'; // warning, danger, success, info
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="assets/dashboard.css">
+    <link rel="icon" href="assets/aquarium.png">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.2/dist/chart.umd.min.js"></script>
 </head>
 <body>
